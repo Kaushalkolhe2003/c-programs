017e093377931a64f3c0226f26a2df6ffad41598
Kaushalkolhe2003 <98825921+Kaushalkolhe2003@users.noreply.github.com>
2022-11-24 22:37:11 +0530

    Update prime_no.c

--- prime_no.c
+++ prime_no.c
@@ -2,17 +2,21 @@
 int main(){
     int n;
     int i;
+    int f=0;
     printf("Enter the number: \n");
     scanf("%d",&n);
-    for(i=2;i<=n;i++){
+    for(i=2;i<n;i++){
         if(n%i==0){
-            printf("Given number is not prime");
+            f=1;
             break;}
-        else{
-            printf("Given number is prime number");
-            break;
-         }
-        
     }
+    if(f==1)
+    {
+        printf("Given number is not prime");
+    }
+    else{
+            printf("Given number is prime number");
+
+         }
     return 0;
 }
